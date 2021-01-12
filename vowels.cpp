5836d770f69d315e08522a1f0d704aa6e0308271
Daniyal_Akram_07 <77106224+daniyal-akram@users.noreply.github.com>
2021-01-12 13:18:58 +0500

    Add files via upload

---
+++ vowels.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{char alphabet;
+cout<<"entre a alphabet"<<endl;
+cin>>alphabet;
+if(alphabet=='a'||alphabet=='e'||alphabet=='i'||alphabet=='o'||alphabet=='u')
+{
+   cout<<"you enterd a vowel";
+}else 
+{
+   cout<<"you enterd a consonant";
+}
+}
